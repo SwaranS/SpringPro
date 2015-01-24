85a2c246dfa91a9a91c61147fcb56f29ac49a029
SwaranS <singhsw@tcd.ie>
2015-01-24 20:55:49 +0000

    Rejected commit: Default (2)

--- src/main/java/com/springapp/mvc/client/client.java
+++ src/main/java/com/springapp/mvc/client/client.java
@@ -1,7 +1,29 @@
 package com.springapp.mvc.client;
 
+import org.springframework.web.client.RestTemplate;
+
 /**
- * Created by Home on 24/01/2015.
+ * Check Domain Status
  */
 public class client {
+
+    static String SERVER_URI = "http://localhost:8080/contacts";
+    public static void main(String[] args){
+        System.out.println(getStatus());
+    }
+    private static Boolean getStatus(){
+        try {
+            return getResponseMessage().equals("Domain Active");
+        }
+        catch(Exception e){
+            return false;
+        }
+    }
+
+
+    //ResponseEntity.getStatusCode()
+    private static String getResponseMessage(){
+    RestTemplate restTemplate = new RestTemplate();
+        return restTemplate.getForObject(SERVER_URI, String.class);
+    }
 }

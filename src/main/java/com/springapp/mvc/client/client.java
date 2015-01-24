6fe3ba620a8cff75731ffb4728e40d890d4f4623
SwaranS <singhsw@tcd.ie>
2015-01-24 21:50:30 +0000

    Status Code

--- src/main/java/com/springapp/mvc/client/client.java
+++ src/main/java/com/springapp/mvc/client/client.java
@@ -1,5 +1,7 @@
 package com.springapp.mvc.client;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
 /**
@@ -9,21 +11,24 @@ public class client {
 
     static String SERVER_URI = "http://localhost:8080/contacts";
     public static void main(String[] args){
-        System.out.println(getStatus());
+        System.out.println(getResponseMessage().is2xxSuccessful());
     }
-    private static Boolean getStatus(){
+
+   /* private static Boolean getStatus(){
         try {
-            return getResponseMessage().equals("Domain Active");
+            return getResponseMessage().equals(200);
         }
         catch(Exception e){
             return false;
         }
-    }
+    }*/
 
 
     //ResponseEntity.getStatusCode()
-    private static String getResponseMessage(){
-    RestTemplate restTemplate = new RestTemplate();
-        return restTemplate.getForObject(SERVER_URI, String.class);
+    private static HttpStatus getResponseMessage(){
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<String> response = restTemplate.getForEntity(SERVER_URI, String.class);
+        /*String restCall = response.getBody();*/
+        return response.getStatusCode();
     }
 }
